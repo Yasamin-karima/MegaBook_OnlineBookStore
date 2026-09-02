debc12f64d1337b3aefcb26c1a029f394c31a1e5
Yasamin Karima <yasamin.krm795@gmail.com>
2026-09-02 20:35:52 +0330

    first_setup

---
+++ backend/MegaBook_Backend/MegaBook_Backend/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for MegaBook_Backend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MegaBook_Backend.settings')
+
+application = get_wsgi_application()
